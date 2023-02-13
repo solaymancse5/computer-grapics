0bd66e54522aac118e09cbd4c49362e5234015f5
MD Solayman <48612738+solaymancse5@users.noreply.github.com>
2023-02-13 19:14:20 +0600

    Update main.cpp

--- home/main.cpp
+++ home/main.cpp
@@ -33,6 +33,10 @@ glVertex3f(0.5, 0.4, 0.0);
 glBegin(GL_LINES);
 glVertex3f(0.5, 0.5, 0.0);
 glVertex3f(0.5, 0.4, 0.0);
+  
+  glBegin(GL_LINES);
+glVertex3f(0.5, 0.5, 0.0);
+glVertex3f(0.4, 0.4, 0.0);
 
 
 glEnd();
