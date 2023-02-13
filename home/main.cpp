87568136fa1f1a1d6a571e23fe4a2cc5bde3a3f1
MD Solayman <48612738+solaymancse5@users.noreply.github.com>
2023-02-13 19:26:47 +0600

    2 lline delet

--- home/main.cpp
+++ home/main.cpp
@@ -30,13 +30,7 @@ glBegin(GL_LINES);
 glVertex3f(0.4, 0.6, 0.0);
 glVertex3f(0.5, 0.4, 0.0);
 
-glBegin(GL_LINES);
-glVertex3f(0.5, 0.5, 0.0);
-glVertex3f(0.5, 0.4, 0.0);
-  
-  glBegin(GL_LINES);
-glVertex3f(0.5, 0.5, 0.0);
-glVertex3f(0.4, 0.4, 0.0);
+
 
 
 glEnd();
